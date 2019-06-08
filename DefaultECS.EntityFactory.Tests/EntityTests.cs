354790609dba1e9444de782eb28d20411686aaeb
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-06-08 13:32:38 +0300

    one more test

--- DefaultECS.EntityFactory.Tests/EntityTests.cs
+++ DefaultECS.EntityFactory.Tests/EntityTests.cs
@@ -150,6 +150,94 @@ namespace DefaultECS.EntityFactory.Tests
             Assert.Equal(123.456, foobarComponent.ValueProperty);
         }
 
+            [Fact]
+        public void Can_compose_entity_hierarchy_that_starts_from_non_root_entity()
+        {
+            var resolver = new DictionaryEntityTemplateResolver(new Dictionary<string, EntityTemplate>
+            {
+                { "ChildFirstLevel",
+                    new EntityTemplate
+                    {
+                        Name = "ChildFirstLevel",
+                        Parent = "RootEntity",
+                        Children = new List<string>{ "ChildSecondLevel" },
+                        Components = new List<ComponentTemplate>
+                        {
+                            new ComponentTemplate
+                            {
+                                Type = typeof(ComponentFactoryTests.StructComponentWithEnum),
+                                Defaults = new Dictionary<string, object>
+                                {
+                                    { "Color", ComponentFactoryTests.Color.Blue }
+                                }
+                            }
+                        }
+                    }
+                },
+                { "ChildSecondLevel",
+                    new EntityTemplate
+                    {
+                        Name = "ChildSecondLevel",
+                        Parent = "ChildFirstLevel",
+                        Components = new List<ComponentTemplate>
+                        {
+                            new ComponentTemplate
+                            {
+                                Type = typeof(ComponentFactoryTests.FoobarComponent),
+                                Defaults = new Dictionary<string, object>
+                                {
+                                    { "ValueProperty", 123.456 }
+                                }
+                            }
+                        }
+                    }
+                },
+                {
+                    "RootEntity",
+                    new EntityTemplate
+                    {
+                        Name = "RootEntity",
+                        Children = new List<string>{ "ChildFirstLevel" },
+                        Components = new List<ComponentTemplate>
+                        {
+                            new ComponentTemplate
+                            {
+                                Type = typeof(ComponentFactoryTests.FoobarStructComponent),
+                                Defaults = new Dictionary<string, object>
+                                {
+                                    { "Value", 123.123 }
+                                }
+                            },
+                            new ComponentTemplate
+                            {
+                                Type = typeof(ComponentFactoryTests.ComponentWithEnum),
+                                Defaults = new Dictionary<string, object>
+                                {
+                                    { "Color", ComponentFactoryTests.Color.Blue }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+            var factory = new EntityFactory(resolver, new World());
+
+            Assert.True(factory.TryCreate(resolver.ResolveEntityTemplate("ChildSecondLevel"), out var rootEntity));
+
+            var firstLevelChildren = rootEntity.GetChildren().ToArray();
+
+            ref var foobarStructComponent = ref rootEntity.Get<ComponentFactoryTests.FoobarStructComponent>();
+            ref var structComponentWithEnum = ref firstLevelChildren[0].Get<ComponentFactoryTests.StructComponentWithEnum>();
+
+            var secondLevelChildren = firstLevelChildren[0].GetChildren().ToArray();
+            ref var foobarComponent = ref secondLevelChildren[0].Get<ComponentFactoryTests.FoobarComponent>();
+
+            Assert.Equal(ComponentFactoryTests.Color.Blue, structComponentWithEnum.Color);
+            Assert.Equal(123.123, foobarStructComponent.Value);
+            Assert.Equal(123.456, foobarComponent.ValueProperty);
+        }
+
         [Fact]
         public void Can_compose_entity_that_inherits()
         {
